4209814e62b0ce3340b61e7cfa1a0fdf0af27839
MyNameIsMyNameYAY <28chang11@gmail.com>
2025-06-29 19:03:22 -0400

    started implementing odometry

--- include/api/odometry/odometry.hpp
+++ include/api/odometry/odometry.hpp
@@ -1,9 +1,24 @@
 #pragma once
-
+#include <cmath>
 namespace aekulib 
 {
     class Odometry
     {
+
+    void y_position(double angle, double arcY){
+        double Tr;
+
+        double y_coord = 2 * std::sin(angle / 2) * (((arcY)/angle) + Tr);
+
+    }
+
+    void x_position(double angle, double arcX){
+        double Ts; 
+
+        double x_coord = 2 * std::sin(angle / 2) * (((arcX)/angle) + Ts);
+
+    }
+
 
     };
 }
