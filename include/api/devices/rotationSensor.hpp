3141b51d93441ae2acf389d2f0138c6af9eb3f48
Averey <avereysku@gmail.com>
2025-07-04 12:52:56 -0400

    finished implementing RotationSensor wrapper class.

--- include/api/devices/rotationSensor.hpp
+++ include/api/devices/rotationSensor.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "pros/rotation.hpp"
 #include "units/angle.h"
 #include "units/angular_velocity.h"
 
@@ -9,31 +10,32 @@ namespace aekulib
 {
     class RotationSensor
     {
-public:
-    explicit RotationSensor(const std::uint8_t port);
+      public:
+        explicit RotationSensor(const std::uint8_t port);
 
-    explicit RotationSensor(const std::uint8_t port, const bool reverse);
+        explicit RotationSensor(const std::uint8_t port, const bool reverse);
 
-    std::int32_t reset();
+        std::int32_t reset();
 
-    std::int32_t resetPosition();
+        std::int32_t resetPosition();
 
-    std::int32_t setReversed(const bool value);
+        std::int32_t setReversed(const bool value);
 
-    std::int32_t setPosition(const degrees<> angle);
+        std::int32_t setPosition(const degrees<> angle);
 
-    degrees<> getPosition() const;
+        degrees<> getPosition() const;
 
-    degrees_per_second<> getVelocity() const;
+        degrees_per_second<> getVelocity() const;
 
         degrees<> getAngle() const;
 
-    bool getReversed() const;
+        bool getReversed() const;
 
-    std::int32_t reverse() const;
+        std::int32_t reverse() const;
 
-    std::int32_t setDataRate(const milliseconds<std::uint8_t> rate);
-private:
-    std::shared_ptr<pros::Rotation> m_rotation;
-}
+        std::int32_t setDataRate(const milliseconds<std::uint8_t> rate);
+
+      private:
+        std::shared_ptr<pros::Rotation> m_rotation;
+    };
 }
