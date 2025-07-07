9194d7edfbe5a75fb26a6cda89ac0941bd675742
MyNameIsMyNameYAY <28chang11@gmail.com>
2025-07-07 18:37:34 -0400

    Implemented getposition and getorientation functions in Odometry class.

--- include/api/odometry/odometry.hpp
+++ include/api/odometry/odometry.hpp
@@ -1,48 +1,52 @@
 #pragma once
-//#include <cmath>
+// #include <cmath>
 #include <numbers>
 #include <vector>
 #include "units/length.h"
+#include "units/angle.h"
 #include "pros/rtos.hpp"
 #include <pros/rotation.hpp>
+#include "Eigen/Core"
+
 using namespace units;
 using namespace units::literals;
 namespace aekulib
 {
     class Odometry
     {
+        //---------------------------------------------------------
+        //                     IMPORTANT
+        //---------------------------------------------------------
+        // REMEMBER:
+        // YOU NEED TO SET CONSTANTS FOR Tl, Tr, Ts, and wheel radius.
+      public:
+        void update();
 
+        Eigen::Vector2<inches<>> getPosition() const;
 
-//---------------------------------------------------------
-//                     IMPORTANT
-//---------------------------------------------------------
-//REMEMBER:
-//YOU NEED TO SET CONSTANTS FOR Tl, Tr, Ts, and wheel radius.
-public:
-void update();
+        degrees<> getOrientation() const;
 
-private:
-void wheel_distance(inches<>& left_dist, inches<>& right_dist, inches<>& back_dist);
-//distance from center to tracking wheels
-const inches<> Tl = 3_in;
-const inches<> Tr = 3_in;
-const inches<> Ts = 3_in;
+      private:
+        void wheel_distance(inches<> &left_dist, inches<> &right_dist, inches<> &back_dist);
+        // distance from center to tracking wheels
+        const inches<> Tl = 3_in;
+        const inches<> Tr = 3_in;
+        const inches<> Ts = 3_in;
 
-//angle change and radius to calculate distance travelled by wheel
-double wheel_angle_left_previous = 0;
-double wheel_angle_right_previous = 0;
-double wheel_angle_back_previous = 0;
+        // angle change and radius to calculate distance travelled by wheel
+        double wheel_angle_left_previous = 0;
+        double wheel_angle_right_previous = 0;
+        double wheel_angle_back_previous = 0;
 
-//total wheel distances
-inches<> left_dist_total = 0_in;
-inches<> right_dist_total = 0_in;
+        // total wheel distances
+        inches<> left_dist_total = 0_in;
+        inches<> right_dist_total = 0_in;
 
-//global orientation
-double orientation = 0;
+        // global orientation
+        double orientation = 0;
 
-//the global x and y coords
-inches<> x_coord = 0_in;
-inches<> y_coord = 0_in;
-
+        // the global x and y coords
+        inches<> x_coord = 0_in;
+        inches<> y_coord = 0_in;
     };
 }
