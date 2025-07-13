e5457911215a515b60e4443852dbd8da0e42fd60
jasonzbai <jasonzbai@gmail.com>
2025-07-13 15:02:18 -0400

    implemented ramsete controller

--- include/api/chassis/control/ramseteController.hpp
+++ include/api/chassis/control/ramseteController.hpp
@@ -1,16 +1,23 @@
 #pragma once
 
-#include "api/odometry/odometry.hpp"
-
+#include "odometry/odometry.hpp"
+#include "units/length.h"
+#include "units/angle.h"
+#include "units/velocity.h"
+#include "units/angular_velocity.h"
 #include <cstdint>
 #include <memory>
-
+#include <cmath>
+using namespace units;
+using namespace units::literals;
 namespace aekulib
 {
     struct RamseteGains
     {
       public:
         RamseteGains(const double ib, const double izeta) : b(ib), zeta(izeta) {}
+        double getB() const { return b; }
+        double getZeta() const { return zeta; }
 
       private:
         double b, zeta;
@@ -22,9 +29,17 @@ namespace aekulib
         explicit RamseteController(const std::shared_ptr<RamseteGains> &iramseteGains,
                                    const std::shared_ptr<Odometry> &iodometry);
 
+        void setTargetValues(inches<> x, inches<> y, degrees<> theta);
         void step();
+        std::pair<inches_per_second<>, degrees_per_second<>> getVelocities();
 
       private:
         std::shared_ptr<Odometry> odometry = nullptr;
+        std::shared_ptr<RamseteGains> ramsetegains;
+        inches<> x_d = 0_in;
+        inches<> y_d = 0_in;
+        degrees<> theta_d = radians(0);
+        inches_per_second<> v = inches_per_second(0);
+        degrees_per_second<> omega = degrees_per_second(0);
     };
 }
--- src/chassis/control/ramseteController.cpp
+++ src/chassis/control/ramseteController.cpp
@@ -6,6 +6,43 @@ namespace aekulib
     RamseteController::RamseteController(const std::shared_ptr<RamseteGains> &iramseteGains,
                                          const std::shared_ptr<Odometry> &iodometry)
     {}
+    void RamseteController::setTargetValues(inches<> x, inches<> y, degrees<> theta)
+    {
+        x_d = x;
+        y_d = y;
+        theta_d = theta;
+    }
 
-    void RamseteController::step() {}
+    void RamseteController::step()
+    {
+        inches<> wheel_circumference;
+        inches<> x_current = odometry->getPosition()[0];
+        inches<> y_current = odometry->getPosition()[1];
+        degrees<> theta_current = odometry->getOrientation() * 180 / std::numbers::pi;
+        double b = ramsetegains->getB();
+        double zeta = ramsetegains->getZeta();
+        // errors
+        inches<> x_error = x_current - x_d;
+        inches<> y_error = y_current - y_d;
+        degrees<> theta_error = theta_current - theta_d;
+        radians<> theta_error_rad = theta_error * std::numbers::pi / 180;
+        // tuning desired values
+        double smallScalar = 0.01;
+        inches_per_second<> vD = inches_per_second(smallScalar * x_error.value());
+        degrees_per_second<> omega_d = degrees_per_second(smallScalar * theta_error.value());
+        // finding k term
+        double k = 2.0 * zeta * std::sqrt(omega_d.value() * omega_d.value() + b * vD.value() * vD.value());
+        // finding linear velocity
+        v = inches_per_second(vD.value() * std::cos(theta_error_rad.value()))
+            + inches_per_second<>(k * x_error.value());
+        // finding angular velocity
+        double omega_rad
+          = (omega_d.value() + k * theta_error.value()
+             + (b * vD.value() * std::sin(theta_error_rad.value()) * y_error.value()) / theta_error.value());
+        omega = degrees_per_second<>(omega_rad * 180 / std::numbers::pi);
+    }
+    std::pair<inches_per_second<>, degrees_per_second<>> RamseteController::getVelocities()
+    {
+        return std::pair<inches_per_second<>, degrees_per_second<>>{v, omega};
+    }
 }
