f825680db48991d16b3f780dd74747ef7f0c9f81
Averey <avereysku@gmail.com>
2025-07-24 12:54:21 -0400

    Add getMaxVelocities() to allow for easier calculations.

--- include/api/chassis/chassisConfiguration.hpp
+++ include/api/chassis/chassisConfiguration.hpp
@@ -4,11 +4,14 @@
 #include "api/devices/imu.hpp"
 #include "pros/abstract_motor.hpp"
 
+#include "units/angular_velocity.h"
 #include "units/length.h"
 #include "units/mass.h"
+#include "units/velocity.h"
 #include <cstddef>
 
 using namespace units;
+using namespace units::literals;
 
 namespace aekulib
 {
@@ -37,16 +40,30 @@ namespace aekulib
                              const kilograms<> imass)
             : wheelDiameter(iwheelDiameter), trackWidth(itrackWidth), gearset(igearset),
               gearRatio(igearRatio), mass(imass)
-        {}
+        {
+            switch(gearset)
+            {
+            case pros::MotorGears::ratio_36_to_1: motorVelocity = 100_rpm;
+            case pros::MotorGears::ratio_18_to_1: motorVelocity = 200_rpm;
+            case pros::MotorGears::ratio_6_to_1: motorVelocity = 600_rpm;
+            case pros::MotorGears::invalid: break;
+            }
+        }
 
         inline inches<> getWheelDiameter() const { return wheelDiameter; }
         inline inches<> getTrackWidth() const { return trackWidth; }
         inline pros::MotorGears getGearset() const { return gearset; }
         inline double getGearRatio() const { return gearRatio; }
+        inline std::pair<inches_per_second<>, radians_per_second<>> getMaxVelocities() const
+        {
+            return {gearRatio * motorVelocity * getWheelDiameter() * M_PI / 1_tr,
+                    (1_rad * gearRatio * motorVelocity * getWheelDiameter() * M_PI / 1_tr) / trackWidth};
+        }
         inline kilograms<> getMass() const { return mass; }
 
       private:
         inches<> wheelDiameter, trackWidth;
+        revolutions_per_minute<> motorVelocity;
         pros::MotorGears gearset;
         kilograms<> mass;
         double gearRatio;
